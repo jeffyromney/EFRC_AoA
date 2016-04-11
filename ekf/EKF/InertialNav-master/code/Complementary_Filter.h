8175de85d4574a12d714a2b7065d2d902a164bb6
CPavanKumar93 <cpavankumar93@yahoo.com>
2016-04-11 10:48:59 -0400

    Update Complementary_Filter.h
    Replaced the integrator with a high-pass filter integrator.

--- ekf/EKF/InertialNav-master/code/Complementary_Filter.h
+++ ekf/EKF/InertialNav-master/code/Complementary_Filter.h
@@ -18,9 +18,8 @@ class Complementary_Filter
     float V_dot_1, V_dot_2;
     float h_1, h_2; // Altitude - ft
     float h_dot_1, h_dot_2; // Change of Altitude - ft/s
-    float Beta_dot_i_1, Beta_dot_i_2;
+    float Beta_dot_i_0, Beta_dot_i_1, Beta_dot_i_2;
     // Depedent variables
-    float Beta_i_0, Beta_i_1, Beta_i_2;
     float Beta_if_0, Beta_if_1, Beta_if_2;
     float Beta_v_0, Beta_v_1, Beta_v_2;
     float Beta_vf_0, Beta_vf_1, Beta_vf_2;
@@ -38,9 +37,6 @@ class Complementary_Filter
     inline float Alpha_Inertial(float sine_G, float Theta, float Beta, float Phi)
     {   return ((-sine_G+sin(Theta)-Beta*sin(Phi))/cos(Phi));   }
 
-    inline float integrate(float un,float un1,float yn)
-    {   return ((T*(un1+un)+2*yn)/2);    }
-
     inline float differentiate(float un, float un1)
     {   return (un1-un/T);   }
 
@@ -48,15 +44,16 @@ class Complementary_Filter
     {   return ((un2+2*un1+un-yn1*2*(1+Z)*(1-Z)-yn*(1-Z)*(1-Z))/(1+Z)/(1+Z)); }
 
     inline float high_pass(float un, float un1, float un2, float yn, float yn1)
-    {   return ((2*Z*(un2*(1/2*Z+1)-un1*Z+un*(1/2*Z-1))-yn1*2*(1+Z)*(1-Z)-yn*(1-Z)*(1-Z))/(1+Z)/(1+Z));   }
+    {   return (2*Z*(un2*(1/2*Z+1)+un1*(2-Z)+un*(1-Z/2))-2*yn*(1+Z)*(1-Z)-yn*(1-Z)*(1-Z))/((1+Z)*(1+Z));   }
     
     public:
     // Class Constructor
     Complementary_Filter(float T = 0.015, float Tau = 3): 
     T(T), Tau(Tau), g(32.2), v(0.0), Theta(0.0), Phi(0.0), ny(0.0), p(0.0), r(0.0),
-    sine_G(0.0), V_1(0.0), V_2(0.0), V_dot_2(0.0), h_1(0.0), h_2(0.0),
-    h_dot_2(0.0), Beta_dot_i_1(0.0), Beta_dot_i_2(0.0), Beta_i_0(0.0), 
-    Beta_i_1(0.0), Beta_i_2(0.0), Beta_if_0(0.0), Beta_if_1(0.0), Beta_if_2(0.0),
+    sine_G(0.0), V_1(0.0), V_2(0.0), V_dot_1(0.0), V_dot_2(0.0), 
+    h_1(0.0), h_2(0.0), h_dot_1(0.0), h_dot_2(0.0), 
+    Beta_dot_i_0(0.0), Beta_dot_i_1(0.0), Beta_dot_i_2(0.0),
+    Beta_if_0(0.0), Beta_if_1(0.0), Beta_if_2(0.0),
     Beta_v_0(0.0), Beta_v_1(0.0), Beta_v_2(0.0), Beta_vf_0(0.0), Beta_vf_1(0.0), 
     Beta_vf_2(0.0), Beta_cf_1(0.0), Beta_cf_2(0.0), Alpha_i_0(0.0), Alpha_i_1(0.0), 
     Alpha_i_2(0.0), Alpha_if_0(0.0), Alpha_if_1(0.0), Alpha_if_2(0.0), Alpha_v_0(0.0), 
@@ -78,9 +75,8 @@ class Complementary_Filter
         // Update for High-Pass Filter
         h_1 = h_2;
 	V_1 = V_2;
+	Beta_dot_i_0 = Beta_dot_i_1;
         Beta_dot_i_1 = Beta_dot_i_2;
-        Beta_i_0 = Beta_i_1;
-        Beta_i_1 = Beta_i_2;
         Beta_if_0 = Beta_if_1;
         Beta_if_1 = Beta_if_2;
         // Update for Low-Pass Filter
@@ -110,9 +106,8 @@ class Complementary_Filter
         sine_G = V_2 > 0 ? h_dot_2/V_2 : (sine_G > 1 ? 1 : (sine_G < - 1 ? -1 : sine_G));
         Alpha_i_2 = cos(Phi) != 0 ? Alpha_Inertial(sine_G, Theta, Beta_cf_1, Phi) : Alpha_i_2;
         Beta_dot_i_2 = V_2 > 0 ? Beta_dot_Inertial(g, V_2, Beta_cf_1, V_dot_2, ny, Theta, Phi, r, Alpha_i_2, p) : Beta_dot_i_2;
-        Beta_i_2 = integrate(Beta_dot_i_1, Beta_dot_i_2, Beta_i_1);
         // High-Pass Filter
-        Beta_if_2 = high_pass(Beta_i_0, Beta_i_1, Beta_i_2, Beta_if_0, Beta_if_1);
+        Beta_if_2 = high_pass(Beta_dot_i_0, Beta_dot_i_1, Beta_dot_i_2, Beta_if_0, Beta_if_1);
         // Vane Computations
         Beta_v_2 = V_2 > 0 ? atan(v/V_2) : Beta_v_2;
         // Low-Pass Filter
