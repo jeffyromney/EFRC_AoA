33981da437234a4a7d31464a68ac8a4bb82f7487
CPavanKumar93 <cpavankumar93@yahoo.com>
2016-04-11 09:13:38 -0400

    Update Complementary_Filter.h
    Added a bunch of robustness checks.

--- ekf/EKF/InertialNav-master/code/Complementary_Filter.h
+++ ekf/EKF/InertialNav-master/code/Complementary_Filter.h
@@ -4,8 +4,6 @@
 
 #include<math.h>
 
-#define PI 3.1415926535897932
-
 class Complementary_Filter
 {
     private:
@@ -17,16 +15,16 @@ class Complementary_Filter
     float v, Theta, Phi, ny, p, r;  //  v (velocity in Yb - ft/s), Theta (Pitch Attitude - rad), Phi (Roll Attitude - rad), ny (Acceleration in Yb), p (Pitch Rate about Xb - rad/s), r (Yaw Rate about Zb - rad/s) 
     float sine_G;   //  sine_G = h_dot/V
     float V_1, V_2; // Total Velocity in Body Axis - ft/s
-	float V_dot_1, V_dot_2;
+    float V_dot_1, V_dot_2;
     float h_1, h_2; // Altitude - ft
-	float h_dot_1, h_dot_2; // Change of Altitude - ft/s
-	float Beta_dot_i_1, Beta_dot_i_2;
+    float h_dot_1, h_dot_2; // Change of Altitude - ft/s
+    float Beta_dot_i_1, Beta_dot_i_2;
     // Depedent variables
     float Beta_i_0, Beta_i_1, Beta_i_2;
     float Beta_if_0, Beta_if_1, Beta_if_2;
     float Beta_v_0, Beta_v_1, Beta_v_2;
     float Beta_vf_0, Beta_vf_1, Beta_vf_2;
-	float Beta_cf_1, Beta_cf_2;
+    float Beta_cf_1, Beta_cf_2;
     float Alpha_i_0, Alpha_i_1, Alpha_i_2;
     float Alpha_if_0, Alpha_if_1, Alpha_if_2;
     float Alpha_v_0, Alpha_v_1, Alpha_v_2;
@@ -34,41 +32,30 @@ class Complementary_Filter
     float Alpha_cf_2;
     
     // Estimates Beta_dot based on the given inputs
-    inline float Beta_dot_Inertial(float g, float V, float Beta, float V_dot, float ny, float Theta, float Phi, float r, float Alpha, float p){
-        float tmp = (g/V*(ny+cos(Theta)*sin(Phi))-Beta*V_dot/V-r+Alpha*p);
-        return (std::isnan(tmp))? 0.0:tmp;
-    }
+    inline float Beta_dot_Inertial(float g, float V, float Beta, float V_dot, float ny, float Theta, float Phi, float r, float Alpha, float p)
+    {   return (g/V*(ny+cos(Theta)*sin(Phi))-Beta*V_dot/V-r+Alpha*p);  }
 
-    inline float Alpha_Inertial(float sine_G, float Theta, float Beta, float Phi){
-        float tmp = ((-sine_G+sin(Theta)-Beta*sin(Phi))/cos(Phi));
-        return (std::isnan(tmp))? 0.0:tmp;
-    }
-    inline float integrate(float un,float un1,float yn){
-        float tmp = ((T*(un1+un)+2*yn)/2);
-        return (std::isnan(tmp))? 0.0:tmp;
-    }
+    inline float Alpha_Inertial(float sine_G, float Theta, float Beta, float Phi)
+    {   return ((-sine_G+sin(Theta)-Beta*sin(Phi))/cos(Phi));   }
 
-    inline float differentiate(float un, float un1, float yn){
-        float tmp ((2*(un1-un)-T*yn)/T);
-        return (std::isnan(tmp))? 0.0:tmp;
-    }
+    inline float integrate(float un,float un1,float yn)
+    {   return ((T*(un1+un)+2*yn)/2);    }
 
-    inline float low_pass(float un, float un1, float un2, float yn, float yn1){
-        float tmp = ((un2+2*un1+un-yn1*2*(1+Z)*(1-Z)-yn*(1-Z)*(1-Z))/(1+Z)/(1+Z));
-        return (std::isnan(tmp))? 0.0:tmp;
-    }
+    inline float differentiate(float un, float un1, float yn)
+    {   return (un1-un/T);   }
 
-    inline float high_pass(float un, float un1, float un2, float yn, float yn1){
-        float tmp = ((2*Z*(un2*(1/2*Z+1)-un1*Z+un*(1/2*Z-1))-yn1*2*(1+Z)*(1-Z)-yn*(1-Z)*(1-Z))/(1+Z)/(1+Z));
-        return (std::isnan(tmp))? 0.0:tmp;
-    }
+    inline float low_pass(float un, float un1, float un2, float yn, float yn1)
+    {   return ((un2+2*un1+un-yn1*2*(1+Z)*(1-Z)-yn*(1-Z)*(1-Z))/(1+Z)/(1+Z)); }
+
+    inline float high_pass(float un, float un1, float un2, float yn, float yn1)
+    {   return ((2*Z*(un2*(1/2*Z+1)-un1*Z+un*(1/2*Z-1))-yn1*2*(1+Z)*(1-Z)-yn*(1-Z)*(1-Z))/(1+Z)/(1+Z));   }
     
     public:
     // Class Constructor
     Complementary_Filter(float T = 0.015, float Tau = 3): 
     T(T), Tau(Tau), g(32.2), v(0.0), Theta(0.0), Phi(0.0), ny(0.0), p(0.0), r(0.0),
-    sine_G(0.0), V_1(0.0), V_2(0.0), V_dot_1(0.0), V_dot_2(0.0), h_1(0.0), h_2(0.0),
-	h_dot_1(0.0), h_dot_2(0.0), Beta_dot_i_1(0.0), Beta_dot_i_2(0.0), Beta_i_0(0.0), 
+    sine_G(0.0), V_1(0.0), V_2(0.0), V_dot_2(0.0), h_1(0.0), h_2(0.0),
+    h_dot_2(0.0), Beta_dot_i_1(0.0), Beta_dot_i_2(0.0), Beta_i_0(0.0), 
     Beta_i_1(0.0), Beta_i_2(0.0), Beta_if_0(0.0), Beta_if_1(0.0), Beta_if_2(0.0),
     Beta_v_0(0.0), Beta_v_1(0.0), Beta_v_2(0.0), Beta_vf_0(0.0), Beta_vf_1(0.0), 
     Beta_vf_2(0.0), Beta_cf_1(0.0), Beta_cf_2(0.0), Alpha_i_0(0.0), Alpha_i_1(0.0), 
@@ -81,22 +68,16 @@ class Complementary_Filter
     void init(float Theta_l, float Phi_l, float ny_l, float r_l, float p_l, float v_l, float Alpha_v_l, float V_1_l, float h_1_l)
     {
         Theta = Theta_l; Phi = Phi_l; ny = ny_l; r = r_l; p = p_l; v = v_l; V_1 = V_1_l; h_1 = h_1_l; Alpha_v_1 = Alpha_v_l;
-        Alpha_i_1 = Alpha_Inertial(sine_G, Theta, Beta_i_1, Phi);
-        if(std::isnan(Alpha_i_1))Alpha_i_1 = 0.0;
-        Beta_dot_i_1 = Beta_dot_Inertial(g, V_1, Beta_i_1, V_dot_1, ny, Theta, Phi, r, Alpha_i_1, p);
-        if(std::isnan(Beta_dot_i_1))Beta_dot_i_1 = 0.0;
-        Beta_v_1 = atan(v/V_1);
-        if(std::isnan(Beta_v_1))Beta_v_1 = 0.0;
-        Beta_cf_1 = 0.0;
+        Alpha_i_1 = cos(Phi) ~= 0 ? Alpha_Inertial(sine_G, Theta, Beta_i_1, Phi) : Alpha_i_1;
+        Beta_dot_i_1 = V_1 > 0 ? Beta_dot_Inertial(g, V_1, Beta_i_1, V_dot_1, ny, Theta, Phi, r, Alpha_i_1, p) : Beta_dot_i_1;
+        Beta_v_1 = V_1 > 0 ? atan(v/V_1) : Beta_v_1;
     }
     
     void update()
     {
         // Update for High-Pass Filter
         h_1 = h_2;
-		V_1 = V_2;
-		h_dot_1 = h_dot_2;
-        V_dot_1 = V_dot_2;
+	V_1 = V_2;
         Beta_dot_i_1 = Beta_dot_i_2;
         Beta_i_0 = Beta_i_1;
         Beta_i_1 = Beta_i_2;
@@ -124,60 +105,27 @@ class Complementary_Filter
     void runFilter(float Theta_l, float Phi_l, float ny_l, float r_l, float p_l, float v_l, float Alpha_v_l, float V_2_l, float h_2_l)
     {
         Theta = Theta_l; Phi = Phi_l; ny = ny_l; r = r_l; p = p_l; v = v_l; V_2 = V_2_l; h_2 = h_2_l; Alpha_v_2 = Alpha_v_l;
-        while(Alpha_v_2 > 2*PI)
-            Alpha_v_2 -= 2*PI;
-        while(Alpha_v_2 < 0)
-            Alpha_v_2 += 2*PI;
-        h_dot_2 = differentiate(h_1, h_2, h_dot_1);
-		V_dot_2 = differentiate(V_1, V_2, V_dot_1);
-        if(V_2 == 0.0)
-            sine_G = 0;
-        else
-    		sine_G = h_dot_2/V_2;
-        if(sine_G > 1)
-            sine_G = 1;
-        if(sine_G < -1)
-            sine_G = -1;
-		Alpha_i_2 = Alpha_Inertial(sine_G, Theta, Beta_cf_1, Phi);
-        while(Alpha_i_2 > 2*PI)
-            Alpha_i_2 -= 2*PI;
-        while(Alpha_i_2 < 0)
-            Alpha_i_2 += 2*PI;
-		Beta_dot_i_2 = Beta_dot_Inertial(g, V_2, Beta_cf_1, V_dot_2, ny, Theta, Phi, r, Alpha_i_2, p);
-		Beta_i_2 = integrate(Beta_dot_i_1, Beta_dot_i_2, Beta_i_1);
-        while(Beta_i_2 > 2*PI)
-            Beta_i_2 -= 2*PI;
-        while(Beta_i_2 < 0)
-            Beta_i_2 += 2*PI;
+        h_dot_2 = differentiate(h_1, h_2);
+        V_dot_2 = differentiate(V_1, V_2);
+        sine_G = V_2 > 0 ? h_dot_2/V_2 : (sine_G > 1 ? 1 : (sine_G < - 1 ? -1 : sine_G));
+        Alpha_i_2 = cos(Phi) ~= 0 ? Alpha_Inertial(sine_G, Theta, Beta_cf_1, Phi) : Alpha_i_2;
+        Beta_dot_i_2 = V_2 > 0 ? Beta_dot_Inertial(g, V_2, Beta_cf_1, V_dot_2, ny, Theta, Phi, r, Alpha_i_2, p) : Beta_dot_i_2;
+        Beta_i_2 = integrate(Beta_dot_i_1, Beta_dot_i_2, Beta_i_1);
         // High-Pass Filter
         Beta_if_2 = high_pass(Beta_i_0, Beta_i_1, Beta_i_2, Beta_if_0, Beta_if_1);
         // Vane Computations
-        if(V_2 == 0.0)
-            Beta_v_2 = 0;
-        else
-            Beta_v_2 = atan(v/V_2);
-        while(Beta_v_2 > 2*PI)
-            Beta_v_2 -= 2*PI;
-        while(Beta_v_2 < 0)
-            Beta_v_2 += 2*PI;
-
+        Beta_v_2 = V_2 > 0 ? atan(v/V_2) : Beta_v_2;
         // Low-Pass Filter
         Beta_vf_2 = low_pass(Beta_v_0, Beta_v_1, Beta_v_2, Beta_vf_0, Beta_vf_1);
         // Complementary Filter (Final)
         Beta_cf_2 = Beta_if_2 + Beta_vf_2;
         // Apha(Inertial) Computations
         Alpha_i_2 = Alpha_Inertial(sine_G, Theta, Beta_cf_2, Phi);
-        while(Alpha_i_2 > 2*PI)
-            Alpha_i_2 -= 2*PI;
-        while(Alpha_i_2 < 0)
-            Alpha_i_2 += 2*PI;
         Alpha_if_2 = high_pass(Alpha_i_0, Alpha_i_1, Alpha_i_2, Alpha_if_0, Alpha_if_1);
         // Alpha(Vane) Computations
         Alpha_vf_2 = low_pass(Alpha_v_0, Alpha_v_1, Alpha_v_2, Alpha_vf_0, Alpha_vf_1);
         // Complementary Filter
         Alpha_cf_2 = Alpha_i_2 + Alpha_vf_2;
-        printf("\n%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f",\
-                    Beta_cf_1,h_dot_2, V_dot_2,  sine_G, Alpha_i_2,  Beta_dot_i_2, Beta_i_2, Beta_if_2, Beta_v_2, Beta_vf_2,  Beta_cf_2, Alpha_i_2, Alpha_if_2,Alpha_vf_2,Alpha_cf_2);
         update();
     }
     
